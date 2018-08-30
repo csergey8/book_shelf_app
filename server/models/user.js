dd473bc4183f194d02dc497233948f5b6783f93e
csergey8 <c.sergey@protonmail.com>
2018-08-30 13:02:50 +0300

    added user routes

--- server/models/user.js
+++ server/models/user.js
@@ -33,6 +33,63 @@ const userScheema = mongoose.Schema({
   }
 });
 
+
+userScheema.pre('save', function(next){
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(SALT_I, (err, salt) => {
+      if(err) return next(err);
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if(err) return next(err);
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+})
+
+userScheema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  })
+}
+
+userScheema.methods.generateToken = function(cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), config.SECRET);
+
+  user.token = token;
+  user.save(function(err, user) {
+    if(err) return cb(err);
+    cb(null, user);
+  });
+}
+
+userScheema.statics.findByToken = function(token, cb) {
+  const user = this;
+
+  jwt.verify(token, config.SECRET, function(err, decode) {
+    user.findOne({"_id": decode, "token": token}, function(err, user) {
+      if(err) return cb(err);
+      cb(null, user);
+    });
+  });
+}
+
+userScheema.methods.deleteToken = function(token, cb) {
+  var user = this;
+
+  user.update({$unset: {token: 1}}, (err, user) => {
+    if(err) return cb(err);
+    cb(null, user);
+  })
+}
+
 const User = mongoose.model('User', userScheema);
 
-module.export = { User };
+module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const config = require('./config/config').get(process.env.NODE_ENV);
 
 const { User } = require('./models/user');
 const { Book } = require('./models/book');
+const { auth } = require('./middleware/auth');
 
 const app = express();
 
@@ -20,6 +21,30 @@ const port = process.env.PORT || 3000;
 
 
 // GET
+app.get('/api/auth', auth, (req, res) => {
+  res.json({
+    isAuth: true,
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname
+  })
+})
+
+
+
+
+
+
+
+app.get('/api/logout', auth, (req, res) => {
+  req.user.deleteToken(req.token, (err, user) => {
+    if(err) return res.status(400).send(err);
+    res.sendStatus(200);
+  });
+});
+
+
 app.get('/api/getBook', (req, res) => {
   let id = req.query.id;
   
@@ -42,6 +67,34 @@ app.get('/api/books', (req, res) => {
   });
 });
 
+app.get('/api/getReviewer', (req, res) => {
+  const id = req.query.id;
+
+  User.findById(id, (err, doc) => {
+    if(err) return res.status(400).send(err);
+
+    res.json({
+      name: doc.name,
+      lastname: doc.lastname
+    })
+  })
+});
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).send(users)
+  })
+})
+
+app.get('/api/user_posts', (req, res) => {
+  console.log(req.query.user);
+  Book.find({ownerId: req.query.user}).exec((err, docs) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).send(docs);
+  })
+})
+
 // POST
 app.post('/api/book', (req, res) => {
   const book = new Book(req.body);
@@ -55,6 +108,38 @@ app.post('/api/book', (req, res) => {
     })
   })
 })
+
+app.post('/api/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({success: false})
+    res.status(200).json({
+      success: true,
+      user: doc
+    });
+  })
+});
+
+app.post('/api/login', (req, res) => {
+  
+  User.findOne({'email': req.body.email}, (err, user) => {
+    if(!user) return res.json({isAuth: false, message: 'This is no such user'});
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({isAuth: false, message: 'Wrong password'})
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie('auth', user.token).json({
+          isAuth: true,
+          id: user._id,
+          email: user.email
+        });
+      })
+    })
+  })
+})
+
+
 
 // UPDATE
 app.post('/api/book_update', (req, res) => {
